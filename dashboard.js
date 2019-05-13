bd092075ddee53497e390dafb43829f5c9b0115f
ash16 <ashnaaggarwal@gmail.com>
2019-05-12 23:58:48 -0400

    Fixed bot response

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,6 @@
 const REGION = "us-west-2";
+var medical_api_token = "";
+
 $(window).scroll(function(){
     if ($(window).scrollTop() >= 300) {
         $('nav').addClass('fixed-header');
@@ -46,6 +48,7 @@ Http.onreadystatechange=(e)=>{
         window.sessionStorage.setItem("username", token['email'])
             window.location.replace("register.html#id_token=" + og_token);
     }else {
+        console.log(response);
         response = JSON.parse(response);
     }
 }
@@ -58,23 +61,38 @@ var params = {
 my_data = []
 
 function init() {
-  const Http = new XMLHttpRequest();
-  const url = 'https://healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InN1a3JpdGkxMC50aXdhcmlAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIyNDExIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMTA4IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6IjEwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IkJhc2ljIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAxOS0wNS0xMSIsImlzcyI6Imh0dHBzOi8vYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTU1NzcwMDU2NiwibmJmIjoxNTU3NjkzMzY2fQ.RVJ4GwkYfMBIxL5oQGk-xacNKJxNyp4asbEAlgncuyE&format=json&language=en-gb';
-  Http.open("GET", url);
-  Http.send();
-  userExists = 0;
-  Http.onreadystatechange=(e)=>{
-    str = Http.responseText.replace(new RegExp('Name', 'g'), 'text');
-    str = str.replace(new RegExp('ID','g'), 'id');
-    my_data = JSON.parse(str);
-    var test = $('#test');
-    $(test).select2({
-        data:my_data.slice(1,10),
-        multiple: true,
-        width: "300px"
-    });
+  var uri = "https://authservice.priaid.ch/login";
+  var api_key = "n3A9J_GMAIL_COM_AUT";
+  var secret_key = "Ft3q8C2MyWw4j5BAr";
+  var computedHash = CryptoJS.HmacMD5(uri, secret_key);
+  var computedHashString = computedHash.toString(CryptoJS.enc.Base64); 
 
-  };
+  const Http1 = new XMLHttpRequest();
+  Http1.open("POST", uri);
+  Http1.setRequestHeader("Authorization", "Bearer " + api_key + ":" + computedHashString)
+  Http1.send();
+  Http1.onreadystatechange=(e)=>{
+    medical_api_token = JSON.parse(Http1.responseText);
+    medical_api_token = medical_api_token['Token'];
+    console.log(medical_api_token);
+    const Http = new XMLHttpRequest();
+    const url = 'https://healthservice.priaid.ch/symptoms?token=' + medical_api_token + '&format=json&language=en-gb';
+    Http.open("GET", url);
+    Http.send();
+    Http.onreadystatechange=(e)=>{
+      str = Http.responseText.replace(new RegExp('Name', 'g'), 'text');
+      str = str.replace(new RegExp('ID','g'), 'id');
+      my_data = JSON.parse(str);
+      var test = $('#test');
+      $(test).select2({
+          data:my_data.slice(1,10),
+          multiple: true,
+          width: "300px"
+      });
+  
+    };
+  }; 
+
 };
 
 init();
@@ -95,12 +113,11 @@ var additionalParams = {
 };
 
 var apigClient = apigClientFactory.newClient({
-  apiKey: ''
+  apiKey: 'v6yONPje7AdEv9jui3Fu35uPMHuuFww419uYp39a'
 });
 
 function submitSymptom() {
   const Http = new XMLHttpRequest();
-  const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InN1a3JpdGkxMC50aXdhcmlAZ21haWwuY29tIiwicm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiIyNDExIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy92ZXJzaW9uIjoiMTA4IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9saW1pdCI6IjEwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcCI6IkJhc2ljIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAxOS0wNS0xMSIsImlzcyI6Imh0dHBzOi8vYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTU1NzcwMjIyOSwibmJmIjoxNTU3Njk1MDI5fQ.P6jEDLX-5RTbcToWFwu4Q_8c7Ez4I9Qqq6zrZfVJJ1w&format=json&language=en-gb';
   const gender = 'female';
   const age = response['age'];
   symptoms = '';
@@ -112,7 +129,7 @@ function submitSymptom() {
         symptoms = symptoms + ',' +  selections[i]["id"];
     }
   }
-  const url = 'https://healthservice.priaid.ch/diagnosis?symptoms=[' + symptoms + ']&gender=' + gender + '&year_of_birth=' + age +'&token='+ token;
+  const url = 'https://healthservice.priaid.ch/diagnosis?symptoms=[' + symptoms + ']&gender=' + gender + '&year_of_birth=' + age +'&token='+ medical_api_token;
   console.log(url);
   Http.open("GET", url);
   Http.send();
@@ -130,30 +147,31 @@ $(test).change(function() {
     $('#selectedText').text(selections);
 });
 
-
-
 $('document').ready(function() {
     $('#msg_send').click(function(e) {
         e.preventDefault();
-            var a = $("<div class='outgoing_msg'><div class='sent_msg'><p>" + $('#userMsg').val() + "</p></div></div>");
-            $("#m_his").append(a);
-            body = {
-            // This is where you define the body of the request,
-                "userMessage": $('#userMsg').val()
-            };
-            apigClient.chatbotPost(params, body, additionalParams)
-                .then(function(data){
-                  // Add success callback code here.
-                  $("#userMsg").val("");
-                  var a = $("<div class='incoming_msg'><div class='incoming_msg_img'><img src='img/icon.jpg'></div><div class='received_msg'><p>" + data["data"]["response"] + "</p></div></div></div>");
-                  $("#m_his").append(a);    
+        var a = $("<div class='outgoing_msg'><div class='sent_msg'><p>" + $('#userMsg').val() + "</p></div></div>");
+        $("#m_his").append(a);
+        body = {
+        // This is where you define the body of the request,
+            "userMessage": $('#userMsg').val()
+        };
+        console.log("yahannn");
+        console.log(body);
+        apigClient.chatbotPost(params, body, additionalParams)
+            .then(function(data){
+                resp = JSON.parse(data['data']['body']);
+                // Add success callback code here.
+                $("#userMsg").val("");
+                var a = $("<div class='incoming_msg'><div class='incoming_msg_img'><img src='img/icon.jpg'></div><div class='received_msg'><p>" + resp['message'] + "</p></div></div></div>");
+                $("#m_his").append(a);    
 
-                  console.log(data["data"]["response"]);
+                //console.log(data["data"]["response"]);
 
-                 // $('#botResponse').val(data["data"]["response"]);
-                }).catch( function(result){
-                  // Add error callback code here.
-                  console.log(result);
-                });
+                // $('#botResponse').val(data["data"]["response"]);
+            }).catch( function(result){
+                // Add error callback code here.
+                console.log(result);
+            });
     });
 }); 
