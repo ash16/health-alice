306604fc05fc51f2b37690b711ceedcdaa16fdcc
Neha Arora <nehaarora@dyn-129-236-216-27.dyn.columbia.edu>
2019-05-13 19:05:55 -0400

    clickable names

--- message.js
+++ message.js
@@ -10,18 +10,23 @@ function fillDoctorDetails(inputJson) {
 
     for (var i = 0; i < num; i++) {
         var row = doctors.insertRow(-1);
-        var cell1 = row.insertCell(0);
+        var cell1 = document.createElement('tr');
+        var val1 = document.createElement('td');
+        val1.setAttribute('style', 'font-size:22px');
+        val1.classList.add('name');
 
         var elem = inputJson[i];
-        var name = elem['firstName'] + ' ' + elem['lastName'];
+        var name = '<a href=# id='+i+'>' + elem['firstName'] + ' ' + elem['lastName'] + '</a>';
         
-        var specialization = '<span class="details">';
+        var specialization = '<span style="font-size:15px">';
         for (var j = 0; j < elem['specialization'].length; j++) {
             specialization = specialization + elem['specialization'][j] + ", ";
         }
         if (specialization.length > 2) specialization = specialization.substr(0, specialization.length - 2);
         specialization = specialization + '</span>';
         var curr_doctor = name + '<br>' + specialization;
-        cell1.innerHTML = curr_doctor;
+        val1.innerHTML = curr_doctor;
+        cell1.appendChild(val1);
+        row.appendChild(cell1);
     }
 }
